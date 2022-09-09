436142851b9a4a78cfff7ef58083ac65ee1b48dc
Aleksandr.ru <aleksandr@aleksandr.ru>
2022-09-09 13:44:47 +0300

    headers and reply-to

--- MultipartEmail.php
+++ MultipartEmail.php
@@ -2,7 +2,8 @@
 
 /**
  * PHP Multipart Mailer
- * @author Aleksandr http://www.aleksandr.ru
+ * @author Aleksandr.ru
+ * @url http://aleksandr.ru
  * @version 1.0   18.04.2012
  * @version 1.0.1 01.10.2013 ('\r\n' replaced with '\n' when building message)
  * @version 1.0.2 06.03.2015 ($add_cid parameter added, html parsing for cid src, 'multipart/related' header in case of embedded images)
@@ -11,20 +12,23 @@
  * @version 1.0.5 20.03.2020 (added backtrace and optional exception for send errors)
  * @version 1.0.6 23.07.2020 (image CID fix when send multiple times)
  * @version 1.0.7 02.10.2020 (composer support)
+ * @version 1.1   09.09.2022 (named headers and reply-to feature)
  */
 class MultipartEmail
 {
+    const EMAIL_REGEXP = "/^(.+) <(.+\@.+)>$/i";
+
 	protected $charset = 'UTF-8';
 	protected $text;
 	protected $html;
 	protected $from;
 	protected $to;
+    protected $reply_to;
 	protected $subject;
 	protected $attachments = array();
 	protected $headers = array();
 
 	private $att_counter = 0;
-	private $hdr_counter = 0;
 	
 	/**
 	* init. set charset for all data (from, to, subject, text and html)
@@ -82,7 +86,7 @@ class MultipartEmail
 
 	/**
 	* set TO address ('some@mail.com' or 'some name <some@mail.com>')
-	* multi addresse can be divided by , or ;
+	* multi addresses can be divided by , or ;
 	* @param string $val
 	* 
 	* @return void
@@ -101,6 +105,27 @@ class MultipartEmail
 	{
 		return $this->to;
 	}
+
+    /**
+     * set REPLY-TO address ('some@mail.com' or 'some name <some@mail.com>')
+     * @param string $val
+     *
+     * @return void
+     */
+    function setReplyTo($val)
+    {
+        $this->reply_to = $val;
+    }
+
+    /**
+     * get REPLY-TO address
+     *
+     * @return string
+     */
+    function getReplyTo()
+    {
+        return $this->reply_to;
+    }
 	
 	/**
 	* set FROM address ('some@mail.com' or 'some name <some@mail.com>')
@@ -145,16 +170,16 @@ class MultipartEmail
 	}
 	
 	/**
-	* add attachement to email
-	* @param string $file filename or data of attachement
+	* add attachment to email
+	* @param string $file filename or data of attachment
 	* @param string $mime_type
-	* @param string $name filename of attachement
+	* @param string $name filename of attachment
 	* @param bool $file_is_data if TRUE use $file as data, if FALSE load data from $file
 	* @param bool $add_cid add content-id to embed in html part by parsing src='...' attribute
 	* 
-	* @return integer attachement id (index)
+	* @return integer attachment id (index)
 	*/
-	function addAttachement($file, $mime_type, $name, $file_is_data = false, $add_cid = false)
+	function addAttachment($file, $mime_type, $name, $file_is_data = false, $add_cid = false)
 	{
 		$this->att_counter++;
 		$this->attachments[$this->att_counter]['data'] = $file_is_data ? $file : file_get_contents($file);
@@ -163,59 +188,94 @@ class MultipartEmail
 		$this->attachments[$this->att_counter]['cid'] = $add_cid ? uniqid('MPM-cid-') : null;
 		if(!$this->attachments[$this->att_counter]['data'] || !$this->attachments[$this->att_counter]['mime_type'] || !$this->attachments[$this->att_counter]['name']) {
 			unset($this->attachments[$this->att_counter]);
-			trigger_error("Failed to add an attachement!", E_USER_WARNING);
+			trigger_error("Failed to add an attachment!", E_USER_WARNING);
 			return false;
 		}
 		return $this->att_counter;
 	}
 	
 	/**
-	* remove attachement 
-	* @param integer $id index of the attachement
+	* remove attachment
+	* @param integer $id index of the attachment
 	* 
 	* @return bool
 	*/
-	function removeAttachement($id)
+    function removeAttachment($id)
 	{
 		if(isset($this->attachments[$id])) {
 			unset($this->attachments[$id]);
 			return true;
 		} else {
-			trigger_error("Attachement not found");
+			trigger_error("Attachment not found");
 			return false;
 		}
 	}
+
+    /**
+     * @deprecated backwards compatibility
+     */
+    function addAttachement($file, $mime_type, $name, $file_is_data = false, $add_cid = false)
+    {
+       return $this->addAttachment($file, $mime_type, $name, $file_is_data, $add_cid);
+    }
+
+    /**
+     * @deprecated backwards compatibility
+     */
+    function removeAttachement($id)
+    {
+        return $this->removeAttachment($id);
+    }
 	
 	/**
-	* add an extra header to email ('Header-name: header-value')
+	* add extra header to email ('Header-name: header-value')
+    * caution! any message header can be redefined with this
 	* @param string $header
 	* 
-	* @return integer header id (index)
+	* @return string header-name
 	*/
 	function addHeader($header)
 	{
-		$this->hdr_counter++;
-		if(!$header) {
-			trigger_error("Can't add empty header!", E_USER_WARNING);
+		if(!preg_match('/^(.+):.+/', $header, $matches)) {
+			trigger_error(sprintf('Invalid header "%s"', $header), E_USER_WARNING);
 			return false;
 		}
-		$this->headers[$this->hdr_counter] = $header;		
-		return $this->hdr_counter;
+        $key = strtolower($matches[1]);
+		$this->headers[$key] = $header;
+		return $key;
 	}
+
+    /**
+     * get extra header value
+     * @param string $key header-name
+     *
+     * @return string
+     */
+    function getHeader($key)
+    {
+        $key = strtolower($key);
+        if(isset($this->headers[$key])) {
+            return $this->headers[$key];
+        } else {
+            trigger_error(sprintf('Header "%s" not found', $key));
+            return false;
+        }
+    }
 	
 	/**
 	* remove an extra header
-	* @param integer $id index of the header
+	* @param string $key header-name
 	* 
 	* @return bool
 	*/
-	function removeHeader($id)
+	function removeHeader($key)
 	{
-		if(isset($this->headers[$id])) {
-			unset($this->headers[$id]);
+        $key = strtolower($key);
+		if(isset($this->headers[$key])) {
+			unset($this->headers[$key]);
 			return true;
 		} else {
-			trigger_error("Header not found");
+			trigger_error(sprintf('Header "%s" not found', $key));
 			return false;
 		}
 	}
@@ -257,15 +317,24 @@ class MultipartEmail
 		}
 		
 		/* headers */
-		
-		$email_regexp = "/^(.+) <(.+\@.+)>$/i";
-		if(preg_match($email_regexp, trim($this->from), $arr)) {
+
+		if(preg_match(self::EMAIL_REGEXP, trim($this->from), $arr)) {
 			$from = "=?UTF-8?B?".base64_encode($this->toUTF8($arr[1]))."?= <{$arr[2]}>";
 		} else {
 			$from = $this->from;
 		}
+
+        if(preg_match(self::EMAIL_REGEXP, trim($this->reply_to), $arr)) {
+            $reply_to = "=?UTF-8?B?".base64_encode($this->toUTF8($arr[1]))."?= <{$arr[2]}>";
+        } elseif($this->reply_to) {
+            $reply_to = $this->reply_to;
+        }
+        else {
+            $reply_to = $from;
+        }
+
 		$to = preg_split("/[,;]/", $this->to);
-		foreach($to as $i=>$t) if(preg_match($email_regexp, trim($t), $arr)) {
+		foreach($to as $i=>$t) if(preg_match(self::EMAIL_REGEXP, trim($t), $arr)) {
 			$to[$i] = "=?UTF-8?B?".base64_encode($this->toUTF8($arr[1]))."?= <{$arr[2]}>";
 		} else {
 			$to[$i] = $t;
@@ -274,14 +343,18 @@ class MultipartEmail
 
 		$subject = "=?UTF-8?B?".base64_encode($this->toUTF8($this->subject))."?=";
 		
-		$headers[] = "From: {$from}";
-		$headers[] = "Reply-To: {$from}"; 
-		$headers[] = "MIME-Version: 1.0";
-		$headers[] = "X-Mailer: PHP/MPM 1.0 by Rebel";		
+		$headers['from']         = "From: {$from}";
+		$headers['reply-to']     = "Reply-To: {$reply_to}";
+		$headers['mime-version'] = "MIME-Version: 1.0";
+		$headers['x-mailer']     = "X-Mailer: PHP/MPM 1.1 by aleksandr.ru";
 	
-		if($multipart && !$related) $headers[] = "Content-Type: multipart/mixed; boundary=\"$boundary_part\"";
-		elseif($multipart && $related) $headers[] = "Content-Type: multipart/related; boundary=\"$boundary_part\"";
-		else $headers[] = "Content-Type: text/plain; charset={$this->charset}\nContent-Transfer-Encoding: base64";
+		if($multipart && !$related) {
+            $headers['content-type'] = "Content-Type: multipart/mixed; boundary=\"$boundary_part\"";
+        } elseif($multipart && $related) {
+            $headers['content-type'] = "Content-Type: multipart/related; boundary=\"$boundary_part\"";
+        } else {
+            $headers['content-type'] = "Content-Type: text/plain; charset={$this->charset}\nContent-Transfer-Encoding: base64";
+        }
 		
 		if(sizeof($this->headers)) $headers = array_merge($headers, $this->headers);
 		$headers = implode("\n", $headers);
@@ -313,7 +386,7 @@ class MultipartEmail
 			$message .= chunk_split(base64_encode($html))."\n";
 		}
 		
-		/* attachements */
+		/* attachments */
 
 		foreach($this->attachments as $att) {
 			$att['name'] = "=?UTF-8?B?".base64_encode($this->toUTF8($att['name']))."?=";
